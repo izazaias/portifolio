a0e604832870403a74803d7a2d31072aaac5a450
Isaias <izazaiasdossantos.is@gmail.com>
2024-08-15 17:49:39 -0300

    Add arquivo js

---
+++ script.js
@@ -0,0 +1,6 @@
+var header = document.getElementById('header');
+var navegacaoHeader = document.getElementById('navegacao_header');
+var content = document.getElementById('content');
+var showSidebar = false;
+
+// parei o video em 30:58 minutos.
